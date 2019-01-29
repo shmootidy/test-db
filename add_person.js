6ad3fc66b5e7a0b17cc45dbb2041a5a508693c1f
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-29 23:05:20 +0000

    Insert query works.

--- add_person.js
+++ add_person.js
@@ -11,3 +11,21 @@ const knex = require('knex')({
     ssl       : settings.ssl
   }
 });
+
+const firstName = 'Keanu';
+const lastName = 'Reeves';
+const birthDate = new Date(1964, 9, 2);
+const newPerson = [
+  { first_name: firstName, last_name: lastName, birthdate: birthDate },
+]
+
+
+knex('famous_people').insert(newPerson).asCallback((err, res) => {
+  console.log(res);
+  knex.destroy();
+})
+
+
+// knex
+//   .insert([{id: 5}, {first_name: 'Keanu'}, {last_name: 'Reeves'}, {birthdate: birthDate}])
+//   .into('famous_people');
