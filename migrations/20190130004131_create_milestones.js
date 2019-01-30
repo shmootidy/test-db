459c5514d31f4088bdd41749cdcb343d6b6951f6
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-30 00:46:18 +0000

    Set up migration. Up function written following Don's example, without Promise.

---
+++ migrations/20190130004131_create_milestones.js
@@ -0,0 +1,11 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('milestones', (table) => {
+    table.string('description');
+    tale.date('date_achieved');
+  })
+};
+
+exports.down = function(knex, Promise) {
+
+};
