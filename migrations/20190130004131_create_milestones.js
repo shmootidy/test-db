dd5b9c609fadced83e51054e461e269833949a1d
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-30 01:10:36 +0000

    Add primary key to new table.

--- migrations/20190130004131_create_milestones.js
+++ migrations/20190130004131_create_milestones.js
@@ -1,6 +1,7 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('milestones', (table) => {
+    table.increments();
     table.string('description');
     table.date('date_achieved');
   })
