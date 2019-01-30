a96e0022b540976282e400565f26122c0b50e9f6
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-30 01:03:06 +0000

    Change file to accept input lines from command.

--- add_person.js
+++ add_person.js
@@ -11,14 +11,22 @@ const knex = require('knex')({
   }
 });
 
-const firstName = 'Keanu';
-const lastName = 'Reeves';
-const birthDate = new Date(1964, 9, 2);
-const newPerson = [
-  { first_name: firstName, last_name: lastName, birthdate: birthDate },
-]
+const input = [firstName, lastName, birthDate] = process.argv.slice(2);
 
-knex('famous_people').insert(newPerson).asCallback((err, res) => {
-  console.log(res);
-  knex.destroy();
-})
+// const firstName = 'Keanu';
+// const lastName = 'Reeves';
+// const birthDate = new Date(1964, 9, 2);
+
+function addPerson (knex, input) {
+  const newPerson = [{ first_name: firstName, last_name: lastName, birthdate: new Date(birthDate) }];
+  knex('famous_people').insert(newPerson).asCallback((err, res) => {
+    console.log(res);
+    knex.destroy();
+  });
+}
+
+if (input) {
+  addPerson(knex, input);
+} else {
+  console.log("Nope!");
+}
